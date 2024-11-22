d8f6a625e2ef3eecd85f3595d04491d008d598e5
wattwenger <50634245+wattwenger@users.noreply.github.com>
2024-11-22 01:08:04 -0500

    update models.py

--- megacellcnc/models.py
+++ megacellcnc/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-
+from megacellcnc.models import Projects
 
 class Projects(models.Model):
     name = models.CharField(max_length=100)
